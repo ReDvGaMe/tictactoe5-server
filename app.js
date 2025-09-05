939143b5c95b162042070020c07d7418adaec764
Kim Ju-Yeong <32252062+ReDvGaMe@users.noreply.github.com>
2025-09-05 10:12:47 +0900

    DB 연결 코드 추가

--- app.js
+++ app.js
@@ -4,10 +4,51 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// DB 설정
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+// 암호화 모듈 설정
+// var bcrypt = require('bcrypt');
+// const saltRounds = 10;
+
+// 세션 설정
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// DB 연결
+async function connectDB() {
+  var databaseUrl = 'mongodb://localhost:27017';
+
+  try {
+    const database = await MongoClient.connect(databaseUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected successfully');
+    app.set('database', database.db('tictactoe'));
+
+    // 연결 종료 처리
+    process.on('SIGINT', async () => {
+      await database.close();
+      console.log('Database connection closed');
+      process.exit(0);
+    });
+  }
+  catch(error){
+    consol.error('Database connection failed: ', error);
+    process.exit(1);
+  }
+}
+
+connectDB().catch(err => {
+  consol.error('Failed to connect to the database: ',err);
+  process.exit(1);
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +64,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
